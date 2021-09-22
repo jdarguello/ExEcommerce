796e344e101787c54b975fba697089f7e9a17195
jdarguello11 <juandavidarguello@gmail.com>
2021-09-22 18:51:04 -0500

    Pull request prueba

--- Checkout_1098730480/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import *   #=> from Checkout_1098349.models import *
-
-class CarritoSerial(serializers.ModelSerializer):
-    class Meta:
-        model = CarritoCompras
-        fields = '__all__'
